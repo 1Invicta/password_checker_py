c18257f05d6e7f0d1079e0b207b3a065d975b3f8
Invicta <invict4.tenebris@gmail.com>
2026-01-25 11:18:32 +0100

    created logging_config.py for logging system

---
+++ password_checker_py/data/logging_config.py
@@ -0,0 +1,34 @@
+# [ logging_config.py ] #
+
+
+# ======== Setup ======== #
+
+# [ Libraries ] #
+import logging
+from pathlib import Path
+from datetime import datetime
+
+LOGS_DIR = Path(__file__).parent / "logs"
+print(LOGS_DIR)
+
+def setup_logging(log_to_file: bool=False, log_dir: str | Path = LOGS_DIR, level=logging.INFO):
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level)
+
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+    
+    if root_logger.handlers:
+        return
+
+    if log_to_file:
+        log_dir = Path(log_dir)
+        log_dir.mkdir(parents=True, exist_ok=True)
+
+        timestamp = datetime.now().strftime("%Y_%m_%d_%H%M%S")
+        logfile = log_dir / f"password_checker_py_{timestamp}.log"
+
+        file_handler = logging.FileHandler(logfile, encoding="utf-8")
+        file_handler.setFormatter(formatter)
+        root_logger.addHandler(file_handler)
+
+        root_logger.info("Logging to file: %s", logfile)
