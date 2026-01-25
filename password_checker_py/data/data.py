f53cd0897628a1f41236ce63ef6f77c7dd06c4ec
Invicta <invict4.tenebris@gmail.com>
2026-01-25 11:06:23 +0100

    renamed file and stored min variables in dictionaries

---
+++ password_checker_py/data/data.py
@@ -0,0 +1,33 @@
+# [ variables.py ] #
+
+
+# ======== Setup ======== #
+
+# ALPHANUMERICAL SETS
+lc = 'abcdefghijklmnopqrstuvwxyz'
+uc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+d = '0123456789'
+s = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
+# MINIMUM REQUIREMENTS
+MIN_LEN = {1: 8, 2: 12, 3: 16}
+MIN_NUM = {1: 2, 2: 3, 3: 5}
+MIN_SPL = {1: 1, 2: 3, 3: 4}
+MIN_REP = {1: 8, 2: 6, 3: 4}
+MIN_ENT = {1: 25, 2: 60, 3: 100}
+
+# BOOLEAN CHECK FLAGS
+cl = False
+cc = False
+cd = False
+cs = False
+cp = False
+cw = False
+ce = False
+
+# BASIC FEEDBACK
+feedback_msgs = {
+    1: "Congratulations! Your password is safe.",
+    0: "Your password is alright, but you can make it stronger.",
+   -1: "Your password is weak! Make it stronger by adding digits and special characters."
+}
